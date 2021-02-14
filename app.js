c2cba62543f1cd84d4b2628c0df0ed94228f73d1
BrandonHarrison01 <49922064+BrandonHarrison01@users.noreply.github.com>
2021-02-13 21:04:56 -0700

    able to rotate ships

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
 document.addEventListener('DOMContentLoaded', () => {
     const gameBoardContainer = document.querySelector('.game-board')
+    const carrier = document.querySelector('.carrier')
+    const battleship = document.querySelector('.battleship')
+    const cruiser = document.querySelector('.cruiser')
+    const submarine = document.querySelector('.submarine')
+    const destroyer = document.querySelector('.destroyer')
+    const rotateButton = document.getElementById('rotate-btn')
+    let isVertical = true
     
     let board = [
         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -23,11 +30,25 @@ document.addEventListener('DOMContentLoaded', () => {
         board.forEach((row, y) => {
             row.forEach((col, x) => {
                 const cell = document.createElement('div')
+                cell.className = "cell"
                 gameBoardContainer.appendChild(cell)
             });
         });
+        console.log('board rendered')
     };
 
+    const rotate = () => {
+        if(isVertical){
+            carrier.classList.toggle('carrier-horizontal')
+            battleship.classList.toggle('battleship-horizontal')
+            cruiser.classList.toggle('cruiser-horizontal')
+            submarine.classList.toggle('submarine-horizontal')
+            destroyer.classList.toggle('destroyer-horizontal')
+        }
+    }
+
+    
     renderBoard()
+    rotateButton.addEventListener("click", rotate)
 })
 
